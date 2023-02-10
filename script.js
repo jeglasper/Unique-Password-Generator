75ef637037e7a220b73fd773af5644e24ef45e6e
jeglasper <109081362+jeglasper@users.noreply.github.com>
2023-02-09 20:55:53 -0600

    add Character selection prompts, creates new string to collect selected characters

--- script.js
+++ script.js
@@ -24,13 +24,45 @@ function generatePassword () {
 
     passwordLength = Number(passwordLength);
 
+    //Makes sure the Password Length Meets the Criteria (8-128 Characters)
     while (Number.isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
         alert('Please select a number between 8 and 128');
         var passwordLength = prompt('How many characters would you like your password to be? Select a Number as small as 8 and as large as 128');
         passwordLength = Number(passwordLength);
-  }
+    }
 
+    //Create Empty String to Hold Characters User Wants to Include in Their New Password
+    var passwordString = '';
 
+    //Adds Prompts for User to Pick What Characters to Include in their new password
+    var confirmUpperCase = confirm('Do you want to include Upper Case Letters in Your Password?\nSelect OK for Yes and Cancel for No');
+    var confirmLowerCase = confirm('Do you want to include Lower Case Letters in Your Password?\nSelect OK for Yes and Cancel for No');
+    var confirmNumbers = confirm('Do you want to include Numbers in Your Password?\nSelect OK for Yes and Cancel for No');
+    var confirmSpecial = confirm('Do you want to include Special Characters in Your Password?\nSelect OK for Yes and Cancel for No');
+
+    //Makes sure at least 1 Character type is selected
+    while (!confirmUpperCase && !confirmLowerCase && !confirmNumbers && !confirmSpecial) {
+        alert('Please Select at Least 1 Type of Character to Include in Your Password');
+        var confirmUpperCase = confirm('Do you want to include Upper Case Letters in Your Password?\nSelect OK for Yes and Cancel for No');
+        var confirmLowerCase = confirm('Do you want to include Lower Case Letters in Your Password?\nSelect OK for Yes and Cancel for No');
+        var confirmNumbers = confirm('Do you want to include Numbers in Your Password?\nSelect OK for Yes and Cancel for No');
+        var confirmSpecial = confirm('Do you want to include Special Characters in Your Password?\nSelect OK for Yes and Cancel for No');
+    }
+
+    //Adds Selected Character Types to Empty String
+    if (confirmUpperCase) {
+    passwordString = passwordString.concat(upperCase);
+    }
+
+    if (confirmLowerCase) {
+    passwordString = passwordString.concat(lowerCase);
+    }
+    if (confirmNumbers) {
+    passwordString = passwordString.concat(numbers);
+    }
+    if (confirmSpecial) {
+    passwordString = passwordString.concat(special);
+    }
 
 }
 
